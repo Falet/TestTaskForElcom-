c7f2af233cb671fbe4287e5e1dd28756ee6b80e7
Falet <miha.m1@yandex.ru>
2020-10-11 18:38:41 +0700

    Update Form1.cs

--- TestTask/TestTask/Form1.cs
+++ TestTask/TestTask/Form1.cs
@@ -12,6 +12,10 @@ namespace TestTask
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             Iinput InputURL = new InputXML();
@@ -30,6 +34,17 @@ namespace TestTask
             {
                 InfoText.Text = "Неправильный URL введите заново";
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
